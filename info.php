03e3c1ef24b00653ce2c5a358aeaf3ef3d51d958
Silverkts <david.mendiganom@gmail.com>
2025-08-22 12:26:53 -0500

    Update info.php

--- info.php
+++ info.php
@@ -5,9 +5,14 @@ $Producto2= $_POST  ['Producto2'];
 $Valor2=$_POST ['Valor2'];
 $Producto3= $_POST  ['Producto3'];
 $Valor3=$_POST ['Valor3'];
-echo "El nombre del producto1 es: ".Producto1;
-echo "El nombre del producto2 es: ".Producto2;
-echo "El nombre del producto3 es: ".Producto3;
-echo "La suma de los 3 precios es: ".Valor1+Valor2+Valor3;
-echo "Y si a ese valor se le añade un descuento del 0.16% da: ".(Valor1+Valor2+Valor3)*0.16;
+
+$suma = $Valor1 + $Valor2 + $Valor3;
+$descuento = $suma * 0.16;
+$totdescuento = $suma - $descuento;
+
+echo "El nombre del producto1 es: $Producto1 <br>";
+echo "El nombre del producto2 es: $Producto2 <br>";
+echo "El nombre del producto3 es: $Producto3 <br>";
+echo "La suma de los 3 precios es: $suma <br>";
+echo "Y si a ese valor se le añade un descuento del 0.16% da: $totdescuento <br>";
 ?>
